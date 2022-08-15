7406b0cf2281ccc8899f697ab4241043b74edd52
pili74 <106436864+pili74@users.noreply.github.com>
2022-08-14 22:52:13 -0300

    no refresca al cambiar de paginas, desde el icono de nike que seria el home y el carrito de compras que seria total

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product';
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+import CheckoutPage from './components/CheckoutPage';
+import CheckoutCard from './components/CheckoutCard';
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path='/checkout-page'>
+            <CheckoutPage />
+          </Route>
+          <Route  path='/'>
+            <Products />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import logo from "../assets/logo.png";
+import { AddShoppingCart } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginBottom: "7rem",
+    },
+    appBar: {
+        backgroundColor: "whitesmoke",
+        boxShadow: "none",
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    button: {
+        marginLeft: theme.spacing(2),
+    },
+    image: {
+        marginRight: "10px",
+        height: "3rem",
+    },
+}));
+
+export default function Navbar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Link to="/">
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <img src={logo} className={classes.image} />
+                        </IconButton>
+                    </Link>
+
+                    <div className={classes.grow} />
+                    <Typography variant="h6" color="textPrimary" component="p">
+                        Hello Guest
+                    </Typography>
+
+                    <div className={classes.button}>
+                        <Button variant="outlined">
+                            <strong>Login</strong>
+                        </Button>
+                    <Link to="checkout-page">
+                        <IconButton aria-label='show cart items' color='inherit'>
+                            <Badge badgeContent={2} color='secondary'>
+                                <AddShoppingCart fontSize="large" color='primary' />
+                            </Badge>
+                        </IconButton>
+                        </Link>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
